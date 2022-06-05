a828d392d65ca626260f521501608ddc50862b96
Half-asian <Half-asian@outlook.com>
2022-06-05 17:33:09 +1200

    Add dome word to ignore double sided shadows

--- Assets/Scripts/3DModels/ModelManager.cs
+++ Assets/Scripts/3DModels/ModelManager.cs
@@ -462,7 +462,7 @@ public class ModelManager
 								node_go.AddComponent<MeshRenderer>();
 								node_go.AddComponent<MeshFilter>();
 								node_go.GetComponent<MeshFilter>().mesh = mesh;
-								if (!node_go.name.Contains("sky") || node_go.name.Contains("skye"))
+								if ((!node_go.name.Contains("sky") && !node_go.name.Contains("dome")) || node_go.name.Contains("skye"))
 									node_go.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.TwoSided;
 								node_go.GetComponent<MeshRenderer>().material = mat;
 							}
@@ -472,7 +472,7 @@ public class ModelManager
 								node_go.AddComponent<SkinnedMeshRenderer>();
 								node_go.GetComponent<SkinnedMeshRenderer>().sharedMesh = mesh;
 								node_go.GetComponent<SkinnedMeshRenderer>().material = mat;
-								if (!node_go.name.Contains("sky") || node_go.name.Contains("skye"))
+								if ((!node_go.name.Contains("sky") && !node_go.name.Contains("dome")) || node_go.name.Contains("skye"))
 									node_go.GetComponent<SkinnedMeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.TwoSided;
 								node_go.GetComponent<SkinnedMeshRenderer>().updateWhenOffscreen = true;
 							}
