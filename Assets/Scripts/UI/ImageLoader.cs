43c00621077360f9de6e9e04c93646a4a75ddb24
Half-asian <Half-asian@outlook.com>
2022-07-03 13:05:11 +1200

    Redid dialogue UI

---
+++ Assets/Scripts/UI/ImageLoader.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+public class ImageLoader : MonoBehaviour
+{
+    protected static Texture2D loadTextureFromApk(string filename)
+    {
+        using (FileStream fs = File.Open(GlobalEngineVariables.apk_folder + "\\assets\\" + filename, FileMode.Open))
+        {
+            byte[] data = new BinaryReader(fs).ReadBytes((int)fs.Length);
+            Texture2D tex = new Texture2D(1, 1, TextureFormat.RGBA32, false);
+            tex.LoadImage(data);
+            tex.name = filename;
+            return tex;
+        }
+    }
+
+    protected static Sprite loadSpriteFromApk(string filename)
+    {
+        using (FileStream fs = File.Open(GlobalEngineVariables.apk_folder + "\\assets\\" + filename, FileMode.Open))
+        {
+            byte[] data = new BinaryReader(fs).ReadBytes((int)fs.Length);
+            Texture2D tex = new Texture2D(1, 1, TextureFormat.RGBA32, false);
+            tex.filterMode = FilterMode.Point;
+            tex.LoadImage(data);
+            tex.name = filename;
+            Sprite sprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), Vector2.one * 0.5f, 100.0f, 0, SpriteMeshType.FullRect);
+            return sprite;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MatImageLoader.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatImageLoader : ImageLoader
+{
+    [SerializeField]
+    private MeshRenderer mesh_renderer;
+    [SerializeField]
+    private string image_name;
+
+    [SerializeField]
+    private bool is_transparent;
+
+    [SerializeField]
+    private string h_name;
+    [SerializeField]
+    private string g_name;
+    [SerializeField]
+    private string r_name;
+    [SerializeField]
+    private string s_name;
+
+    void Awake()
+    {
+        if (is_transparent)
+            mesh_renderer.material = Resources.Load("Materials/TransparentLit") as Material;
+
+        Texture2D texture = null;
+        if (!string.IsNullOrEmpty(h_name))
+        {
+            switch (Player.local_avatar_house)
+            {
+                case "hufflepuff":
+                    texture = loadTextureFromApk(h_name);
+                    break;
+                case "gryffindor":
+                    texture = loadTextureFromApk(g_name);
+                    break;
+                case "ravenclaw":
+                    texture = loadTextureFromApk(r_name);
+                    break;
+                case "slytherin":
+                    texture = loadTextureFromApk(s_name);
+                    break;
+            }
+        }
+        else
+            texture = loadTextureFromApk(image_name);
+
+        mesh_renderer.material.SetTexture("_BaseColorMap", texture);
+
+
+    }
+}
